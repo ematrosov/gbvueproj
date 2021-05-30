963e26aff22e378cfccf32530beb26a3927fb095
matrosov <egor.matrosov@gmail.com>
2021-05-30 10:41:09 +0700

    add hw

---
+++ src/Store/index.js
@@ -0,0 +1,48 @@
+import Vue from 'vue'
+import { set } from 'vue/types/umd'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+
+    state: {
+        paymentsList: []
+
+    },
+    mutations: {
+        setPaymentsListDta(state, payload) {
+            state.paymentsList = payload
+        }
+    },
+    getters: {
+        getPaymentsList: state => state.paymentsList,
+        getPaymentsListFullPrice: state => {
+            return state.paymentsList
+                .reduce((res, cur) => res + cur.price, 0)
+        }
+
+    },
+    actions: {
+        fetchData({ commit }) {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    const items = []
+                    for (let i = 1; i < 105; i++) {
+                        items.push({
+                            date: "13.05.2021",
+                            category: "Education",
+                            price: i
+                        })
+                    }
+                    resolve(items)
+                }, 2000)
+
+            })
+                .then(res => {
+                    commit('setPaymentsListData', res)
+
+                })
+        }
+    }
+})
